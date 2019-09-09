6d4b2b9dfe5dff679804405f994e0aefbc3a910a
Yura M <ymitsevich@gmail.com>
2019-09-09 16:39:50 +0300

    extended e2e tests

--- tests/e2e/WebLinkControllerTest.php
+++ tests/e2e/WebLinkControllerTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\e2e;
 
+use App\Links\CompressedLink;
+use App\Links\Generators\LinkHashGenerator;
 use App\Links\Services\CompressedLinkServiceInterface;
 use App\User;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Http\Response;
 use Tests\TestCase;
@@ -22,18 +25,31 @@ class WebLinkControllerTest extends TestCase
      */
     private $linkService;
 
+    /**
+     * @var LinkHashGenerator
+     */
+    private $linkHashGenerator;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->user = factory(User::class)->create();
         $this->linkService = app(CompressedLinkServiceInterface::class);
+        $this->linkHashGenerator = app(LinkHashGenerator::class);
     }
 
     public function testProcessWithCompressedLink()
     {
         $assertUrl = 'https://github.com/ymitsevich';
         $fullLink = $this->linkService->setUser($this->user)->buildCompressed($assertUrl);
+        $hash = explode('/', $fullLink);
+        $hash = end($hash);
+        $id = $this->linkHashGenerator->getNumberByHash($hash);
+        $this->assertDatabaseHas('compressed_links', [
+            'id' => $id,
+        ]);
+
         $response = $this->get($fullLink);
         $response->assertRedirect($assertUrl);
         $this->assertEquals(Response::HTTP_FOUND, $response->getStatusCode());
